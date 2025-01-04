b72dbac0eef3332f3cfdcc5f2d42eec8d9719343
l.feng <43399351+msclock@users.noreply.github.com>
2025-01-04 11:14:55 +0800

    build(deps): sync bit7z and 7zip from vcpkg (#29)
    Signed-off-by: l.feng <43399351+msclock@users.noreply.github.com>

--- cmake/vcpkg/ports/7zip/portfile.cmake
+++ cmake/vcpkg/ports/7zip/portfile.cmake
@@ -6,21 +6,12 @@ vcpkg_from_github(
     REF "${VERSION}"
     SHA512 dc0241ed96907965445550912d1171fe32230a52997b089558a4cc73a662fc6a17940db8dcb0794b805268964899d9e5a48ddb444e92b56fd243bbaa17c20a1c
     HEAD_REF main
+    PATCHES
+        fix_timespec_get_broken_on_android.patch
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/7zip-config.cmake.in" DESTINATION "${SOURCE_PATH}")
-
-vcpkg_cmake_get_vars(cmake_vars_file)
-include("${cmake_vars_file}")
-
-if(VCPKG_TARGET_IS_ANDROID)
-    message(STATUS "Disable TIME_UTC on android")
-    vcpkg_replace_string("${SOURCE_PATH}/CPP/Windows/TimeUtils.cpp"
-        [[if defined(TIME_UTC)]]
-        [[if defined(TIME_UTC) && !defined(__ANDROID__)]]
-    )
-endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
